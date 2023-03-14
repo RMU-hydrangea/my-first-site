1cae4ac41f2b6ac799f55318af7201a2e88072fa
yusuke <hydrangea.or.la@gmail.com>
2023-03-14 15:08:24 +0900

    add view and template for detailed blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('', views.test, name='test')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
